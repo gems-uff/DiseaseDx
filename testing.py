c27921bc61d8781f17d3b3e09aeda299861bdf59
Matheus Folly <matheusfolly@id.uff.br>
2024-11-02 19:26:52 -0300

    testing query statements from quickstart guide

--- testing.py
+++ testing.py
@@ -50,8 +50,38 @@ with Session(engine) as session:
 session = Session(engine)
 select_stmt = select(Doenca) # can use select().join().where().where() etc | select(Doenca).where(Doenca.name == "Familial Mediterranean Fever")
 
+# Session.scalars executes the query and returns the result as a scalar
 for doenca in session.scalars(select_stmt):
-    print(doenca)
+    print(f"\n{doenca}")
+print("")
+
+# Update query
+stmt = select(Doenca).where(Doenca.name == "Diabetes Mellitus")
+diabetes = session.scalars(stmt).one()
+
+# Update the name of the disease
+diabetes.name = "Diabetes Type 2"
+session.commit()
+
+# Verify the update
+for doenca in session.scalars(select_stmt):
+    print(f"\n{doenca}")
+print("")
+
+# Delete query
+# If I had a class User and a class Address, and User have an Address column, I could delete the address of a user and the address would also be deleted from its own table (cascade)
+# I would do a user.address.remove(user_address) and then session.commit(), or if I want to only remove the specific object, I could do:
+session.delete(diabetes)
+
+# Commit the deletion
+session.commit()
+
+# Verify the deletion
+for doenca in session.scalars(select_stmt):
+    print(f"\n{doenca}")
+print("")
 
 # Close the session
 session.close()
+
+# End of quickstart

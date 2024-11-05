81ccb5935c749c8bcb7ad85d64e9b3fc2ae35e40
Matheus Folly <matheusfolly@id.uff.br>
2024-11-05 00:52:31 -0300

    adding joined_inheritance.py for testing joined table inheritance logic

--- joined_inheritance.py
+++ joined_inheritance.py
@@ -4,17 +4,21 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship,
 import os
 from urllib.parse import quote_plus
 
-# Configurações de conexão com o MySQL
+
+# Configurações de conexão com o MySQL ou SQLite
 username = os.getenv('MYSQL_USER')
 password = quote_plus(os.getenv('MYSQL_PASS'))
 server = "localhost"
 port = "3306"
 dbname = "diseasedx_test"
 connection_string = f"mysql+mysqlconnector://{username}:{password}@{server}:{port}/{dbname}"
+# connection_string = "sqlite://"  # Se quiser usar em memoria
+
 
 # Classe base declarativa
 class Base(DeclarativeBase):
     pass
+
 
 class Expressao(Base):
     __tablename__ = "expressao"
@@ -24,6 +28,7 @@ class Expressao(Base):
     __mapper_args__ = {
         "polymorphic_on": "expr"
     }
+
 
 class And(Expressao):
     __tablename__ = "and"
@@ -35,13 +40,16 @@ class And(Expressao):
     right_expr = relationship("Expressao", foreign_keys=[right_id])
 
     __mapper_args__ = {
-        "inherit_condition": id == Expressao.id,
+        "inherit_condition": id == Expressao.id, # For joined table inheritance, 
+                                                 # a SQL expression which will define how the two tables are joined; 
+                                                 # defaults to a natural join between the two tables.
     }
 
     def __init__(self, left_expr, right_expr):
         super().__init__(expr=f"({left_expr.expr} AND {right_expr.expr})")
         self.left_expr = left_expr
         self.right_expr = right_expr
+
 
 class Or(Expressao):
     __tablename__ = "or"
@@ -61,6 +69,7 @@ class Or(Expressao):
         self.left_expr = left_expr
         self.right_expr = right_expr
 
+
 class Sintoma(Expressao):
     __tablename__ = "sintoma"
     id: Mapped[int] = mapped_column(ForeignKey("expressao.id"), primary_key=True)
@@ -73,6 +82,7 @@ class Sintoma(Expressao):
     def __init__(self, name):
         super().__init__(expr=name)
         self.name = name
+
 
 class Resultado(Expressao):
     __tablename__ = "resultado"
@@ -87,6 +97,7 @@ class Resultado(Expressao):
         super().__init__(expr=name)
         self.name = name
 
+
 # Criando engine para conectar ao banco de dados
 engine = create_engine(connection_string, echo=True)
 
@@ -94,22 +105,52 @@ Base.metadata.create_all(engine)  # Cria as tabelas com o esquema atualizado
 
 # Iniciando uma sessão para inserir dados no banco de dados
 with Session(engine) as session:
-    # Exemplo de inserção de dados
-    sintoma1 = Sintoma(name="Dor de cabeça")
-    sintoma2 = Sintoma(name="Febre")
-    resultado1 = Resultado(name="Ferro alto")
-    or_expr = Or(left_expr=sintoma2, right_expr=resultado1)
-    and_expr = And(left_expr=sintoma1, right_expr=or_expr)
 
-    # Inserindo apenas a expressão final
-    session.add(and_expr)
+    # Criando os objetos previamente para nao duplicar a entrada no banco de dados
+    dor_de_cabeca = Sintoma(name="Dor de cabeça")
+    febre = Sintoma(name="Febre")
+    ferro_alto = Resultado(name="Ferro alto")
+    variante_mefv_patogenica = Resultado(name="Variante MEFV patogênica")
+    vus_de_mefv = Resultado(name="VUS de MEFV")
+    dor_no_peito = Sintoma(name="Dor no peito")
+
+    # Exemplo de inserção de dados syntax tree
+    diseasedx_expr = And(
+        left_expr=dor_de_cabeca,
+        right_expr=Or(
+            left_expr=febre,
+            right_expr=ferro_alto
+        )
+    )
+
+    fmf_expr = Or(
+        left_expr=And(
+            left_expr=variante_mefv_patogenica,
+            right_expr=febre
+        ),
+        right_expr=And(
+            left_expr=vus_de_mefv,
+            right_expr=And(
+                left_expr=dor_de_cabeca,
+                right_expr=dor_no_peito
+            )
+        )
+    )
+
+    # Exemplo de inserção de dados linha a linha
+    # or_expr = Or(left_expr=febre, right_expr=ferro_alto)
+    # diseasedx_expr = And(left_expr=dor_de_cabeca, right_expr=or_expr)
+
+    # Inserir as expressoes no banco de dados
+    session.add(diseasedx_expr)
+    session.add(fmf_expr)
     session.commit()
 
-    # Buscando a expressão no banco de dados
-    expr_query = select(Expressao).where(Expressao.expr == and_expr.expr)
+    # Buscando a expressão no banco de dados usando o objeto criado
+    expr_query = select(Expressao).where(Expressao.expr == diseasedx_expr.expr)
     result = session.execute(expr_query).scalars().first()
+    print(result.expr)
 
-    if result:
-        print(f"Expressão encontrada: {result.expr}")
-    else:
-        print("Expressão não encontrada")
+    expr_query = select(Expressao).where(Expressao.expr == fmf_expr.expr)
+    result = session.execute(expr_query).scalars().first()
+    print(result.expr)
---
+++ quickstart.py
@@ -0,0 +1,85 @@
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String, create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
+
+class Base(DeclarativeBase):
+    pass
+
+class Doenca(Base):
+    __tablename__ = "doenca"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, )
+    name: Mapped[str] = mapped_column(String(30))
+
+    def __repr__(self) -> str: # Not necessary, but useful for debugging
+        return f"Doenca(id={self.id!r}, name={self.name!r})"
+
+# Testing object creation like OOP
+fmf = Doenca(name="Familial Mediterranean Fever")
+
+print(fmf)
+print(fmf.id)
+print(fmf.name)
+
+diabetes = Doenca()
+diabetes.name = "Diabetes Melitus"
+
+print(diabetes)
+print(diabetes.id)
+print(diabetes.name)
+
+# Create a SQLite database in memory
+engine = create_engine("sqlite://", echo=True)
+
+# Create the table
+Base.metadata.create_all(engine)
+
+# Begin a session to insert data in the database
+with Session(engine) as session:
+    diabetes = Doenca(
+        name="Diabetes Mellitus"
+    )
+
+    fmf = Doenca(
+        name="Familial Mediterranean Fever"
+    )
+
+    session.add_all([diabetes, fmf])
+    session.commit()
+
+# Simple select query
+session = Session(engine)
+select_stmt = select(Doenca) # can use select().join().where().where() etc | select(Doenca).where(Doenca.name == "Familial Mediterranean Fever")
+
+# Session.scalars executes the query and returns the result as a scalar
+for doenca in session.scalars(select_stmt):
+    print(f"\n{doenca}")
+print("")
+
+# Update query
+stmt = select(Doenca).where(Doenca.name == "Diabetes Mellitus")
+diabetes = session.scalars(stmt).one()
+
+# Update the name of the disease
+diabetes.name = "Diabetes Type 2"
+session.commit()
+
+# Verify the update
+for doenca in session.scalars(select_stmt):
+    print(f"\n{doenca}")
+print("")
+
+# Delete query
+# If I had a class User and a class Address, and User have an Address column, I could delete the address of a user and the address would also be deleted from its own table (cascade)
+# I would do a user.address.remove(user_address) and then session.commit(), or if I want to only remove the specific object, I could do:
+session.delete(diabetes)
+
+# Commit the deletion
+session.commit()
+
+# Verify the deletion
+for doenca in session.scalars(select_stmt):
+    print(f"\n{doenca}")
+print("")
+
+# Close the session
+session.close()

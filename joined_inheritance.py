560e8098a2570ab68bdb06b3f3fba6b820d602f1
Matheus Folly <matheusfolly@id.uff.br>
2024-11-24 22:40:33 -0300

    attempt on ao_menos class

--- joined_inheritance.py
+++ joined_inheritance.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlalchemy import ForeignKey, String, Float, create_engine, select
+from sqlalchemy import ForeignKey, String, Float, Integer, Table, Column, create_engine, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 import os
 from urllib.parse import quote_plus
@@ -22,10 +22,20 @@ class Base(DeclarativeBase):
     pass
 
 
+ao_menos_expressoes = Table(
+    "ao_menos_expressoes",
+    Base.metadata,
+    Column("ao_menos_id", Integer, ForeignKey("ao_menos.id"), primary_key=True),
+    Column("expressao_id", Integer, ForeignKey("expressao.id"), primary_key=True)
+)
+
+
 class Expressao(Base):
     __tablename__ = "expressao"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     type: Mapped[str] = mapped_column(String(255)) # Coluna discriminadora para o tipo de expressão
+
+    ao_menos_expr: Mapped[list["AoMenos"]] = relationship("AoMenos", secondary=ao_menos_expressoes, back_populates="expressoes")
 
     __mapper_args__ = {
         "polymorphic_identity": "expressao",
@@ -82,6 +92,24 @@ class Or(Expressao):
         return f"{self.__class__.__name__}({self.left_expr!r}, {self.right_expr!r})"
 
 
+class AoMenos(Expressao):
+    __tablename__ = "ao_menos"
+    id: Mapped[int] = mapped_column(ForeignKey("expressao.id"), primary_key=True)
+    qtd: Mapped[int] = mapped_column(Integer)
+
+    expressoes: Mapped[list["Expressao"]] = relationship("Expressao", secondary=ao_menos_expressoes, back_populates="ao_menos_expr")
+
+    __mapper_args__ = {
+        "polymorphic_identity": "ao_menos"
+    }
+
+    def __init__(self, qtd: int, expressoes: list[Expressao]):
+        self.qtd = qtd
+        self.expressoes = expressoes
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.qtd!r})({self.expressoes!r})"
+
 
 class Sintoma(Expressao):
     __tablename__ = "sintoma"
@@ -114,7 +142,7 @@ class Doenca(Base):
     __tablename__ = "doenca"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255))
-    diagnosticos: Mapped[list["Diagnostico"]] = relationship("Diagnostico", back_populates="doenca")
+    diagnostico: Mapped["Diagnostico"] = relationship("Diagnostico", back_populates="doenca") # Doenca tem 1 Diagnostico e Diagnostico tem 1 Expressao que tem 1 ou mais Expressoes
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.name!r})"
@@ -128,7 +156,7 @@ class Diagnostico(Base):
     doenca_id: Mapped[int] = mapped_column(ForeignKey("doenca.id"))
     expressao_id: Mapped[int] = mapped_column(ForeignKey("expressao.id"))
     
-    doenca: Mapped[Doenca] = relationship("Doenca", back_populates="diagnosticos")
+    doenca: Mapped[Doenca] = relationship("Doenca", back_populates="diagnostico")
     expressao: Mapped[Expressao] = relationship("Expressao")
     
     def __repr__(self):
@@ -161,14 +189,17 @@ with Session(engine) as session:
     # Criando a expressão para a doença Familial Mediterranean Fever
     fmf_expr = Or(
         And(
-            dor_no_peito,
-            variante_mefv_patogenica
+            variante_mefv_patogenica,
+            AoMenos(
+                1,
+                [dor_no_peito, dor_no_abdome, artrite_no_ombro]
+            )
         ),
         And(
             vus_de_mefv,
-            And(
-                dor_no_abdome,
-                artrite_no_ombro
+            AoMenos(
+                2,
+                [dor_no_peito, dor_no_abdome, artrite_no_ombro]
             )
         )
     )
@@ -178,7 +209,7 @@ with Session(engine) as session:
     diag = Diagnostico(sensibilidade=0.94, especificidade=0.95, acuracia=0.98, doenca=fmf, expressao=fmf_expr)
 
     # Verificando o objeto criado em memoria
-    print("\n--- Objeto Doenca FMF criado em memoria antes de ser adicionado ao banco de dados:")
+    print("\n### Objeto Doenca FMF criado em memoria antes de ser adicionado ao banco de dados:")
     print(fmf)
     print(type(fmf))
     print()
@@ -192,14 +223,14 @@ with Session(engine) as session:
     doenca = session.scalars(statement).first()
 
     # Verificando o objeto Doenca recuperado do banco de dados
-    print("\n--- Objeto Doenca FMF recuperado do banco de dados:")
+    print("\n### Objeto Doenca FMF recuperado do banco de dados:")
     print(doenca)
     print(type(doenca))
     print()
 
     # Navegando para o Diagnostico a partir da Doenca
-    diagnostico = doenca.diagnosticos[0]
-    print("\n--- Diagnostico a partir da Doenca FMF:")
+    diagnostico = doenca.diagnostico
+    print("\n### Diagnostico a partir da Doenca FMF:")
     print(diagnostico)
     print()
     print(f"Nome da doença: {diagnostico.doenca}")
@@ -211,7 +242,18 @@ with Session(engine) as session:
 
     # Navegando para a Expressao a partir do Diagnostico
     expr = diagnostico.expressao
-    print("\n--- Expressao a partir do Diagnostico:")
+    print("\n### Expressao a partir do Diagnostico:")
     print(expr)
     print(type(expr))
     print()
+
+    # Navegando para as Expressoes do AoMenos a partir da Expressao do Diagnostico
+    ao_menos = expr.left_expr.right_expr # Rever se tem como navegar para ca melhor
+    print("\n### Expressoes do AoMenos a partir da Expressao:")
+    print(ao_menos)
+    print(type(ao_menos))
+    print()
+
+    print(ao_menos.qtd)
+    for expressao in ao_menos.expressoes:
+        print(expressao)

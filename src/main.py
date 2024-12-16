854741dbd62db40ac92e95c1bd52c63f3e58d7ea
Matheus Folly <matheusfolly@id.uff.br>
2024-12-16 12:53:14 -0300

    adding selectbox to choose a specific symptom to test it

--- src/main.py
+++ src/main.py
@@ -9,34 +9,13 @@ import streamlit as st
 # Criando o banco de dados e as tabelas
 engine = DatabaseConfig().init_db()
 
-def walk(obj):
-    deque = [obj]
-
-    seen = set()
-
-    while deque:
-        obj = deque.pop(0)
-        if obj in seen:
-            continue
-        else:
-            seen.add(obj)
-            yield obj
-        insp = inspect(obj)
-        for relationship in insp.mapper.relationships:
-            related = getattr(obj, relationship.key)
-            if relationship.uselist:
-                deque.extend(related)
-            elif related is not None:
-                deque.append(related)
 
 # Função para buscar todos os sintomas do banco de dados
 def get_all_sintomas():
     with Session(engine) as session:
         statement = select(Sintoma).options(
             joinedload(Sintoma.manifestacao),
-            joinedload(Sintoma.regiao_do_corpo.of_type(RegiaoComposta)).joinedload(RegiaoComposta.regioes),
-            joinedload(Sintoma.regiao_do_corpo.of_type(RegiaoDoCorpo)).joinedload(RegiaoDoCorpo.regiao_composta),
-            joinedload(Sintoma.ao_menos_expr).joinedload(AoMenos.expressoes)
+            joinedload(Sintoma.regiao_do_corpo)
         )
         sintomas = session.scalars(statement).unique().all()
         return sintomas
@@ -46,17 +25,10 @@ def get_doencas_by_sintoma(sintoma):
     with Session(engine) as session:
         sintoma_id = sintoma.id
         sintoma = session.get(Sintoma, sintoma_id)
-        print(f"\n- Sintoma recuperado do banco de dados:", sintoma)
-        expressoes = sintoma.ao_menos_expr
-        print(f"\n- Expressoes do sintoma recuperado do banco de dados:")
-        for expr in expressoes:
-            print(expr)
 
         # Função recursiva para verificar se uma expressão contém outra expressão
         def contains_expression(expr, target_expr):
-            print(f"\n- Verificando se a expressão: {expr} \n- Contém a expressão: {target_expr}")
             if expr == target_expr:
-                print(f"Expressão encontrada dentro de: {expr}")
                 return True
             if isinstance(expr, (And, Or)):
                 return contains_expression(expr.left_expr, target_expr) or contains_expression(expr.right_expr, target_expr)
@@ -69,24 +41,15 @@ def get_doencas_by_sintoma(sintoma):
             joinedload(Diagnostico.doenca),
             joinedload(Diagnostico.expressao)
         ).all()
-        print(f"\n- Diagnósticos recuperados do banco de dados:")
-        for diag in diagnosticos:
-            print(diag)
 
         diagnosticos_filtrados = []
         for diag in diagnosticos:
-            for expr in expressoes:
-                if contains_expression(diag.expressao, sintoma):
-                    diagnosticos_filtrados.append(diag)
-                    break
+            if contains_expression(diag.expressao, sintoma):
+                diagnosticos_filtrados.append(diag)
             
-        # diagnosticos_filtrados = [
-        #     diag for diag in diagnosticos
-        #     if any(contains_expression(diag.expressao, expr) for expr in expressoes)
-        # ]
-
-        # Obter todas as doenças ligadas a esses diagnósticos
-        doencas = [diag.doenca for diag in diagnosticos_filtrados]
+        doencas = []
+        for diag in diagnosticos_filtrados:
+            doencas.append(diag.doenca)
 
         return doencas
 
@@ -94,5 +57,9 @@ def get_doencas_by_sintoma(sintoma):
 sintomas = get_all_sintomas()
 st.write(sintomas)
 
-doencas = get_doencas_by_sintoma(sintomas[2])
+# Create a dropdown option to select a specific symptom to search for diseases
+sintoma = st.selectbox("Selecione o sintoma", [sintoma for sintoma in sintomas])
+
+# Only run get_doencas_by_sintoma after selecting at least 1 symptom from the multiselect
+doencas = get_doencas_by_sintoma(sintoma)
 st.write(doencas)
